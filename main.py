a4b796ea4873b67a888c9cb75f686ddc6e2292ee
Ne0Tech <pedigzav001@warren.k12.in.us>
2025-01-17 09:54:56 -0500

    1st

--- main.py
+++ main.py
@@ -7,3 +7,14 @@ modified_string = modified_string.replace(" ", "_")
 print(modified_string)
 
 print(len(modified_string))
+
+user_input = input("Enter a string: ")
+
+processed_string = user_input.replace(" ", "").lower()
+
+is_palindrome = processed_string == processed_string[::-1]
+
+if is_palindrome:
+    print("The string is a palindrome.")
+else:
+    print("The string is not a palindrome.")
